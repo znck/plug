705b67d7ca5379c98b11b4045b6bfee255f9be22
Rahul Kadyan <hi@znck.me>
2016-02-28 04:03:26 +0530

    better foreign key

---
+++ src/Traits/FixForeignKey.php
@@ -0,0 +1,15 @@
+<?php namespace Znck\Plug\Eloquent\Traits;
+
+trait FixForeignKey
+{
+
+    /**
+     * Get the default foreign key name for the model.
+     *
+     * @return string
+     */
+    public function getForeignKey()
+    {
+        return str_singular($this->getTable()).'_id';
+    }
+}
